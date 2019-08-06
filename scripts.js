2a85451c691a1072b4c4eacd64a2ee146dc7fd93
Alistair Ponce <alistair.ponce@gmail.com>
2019-08-06 09:39:35 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,3 +8,11 @@ $('.ff-slide-popup .close-btn').click(function (e) {
 	e.preventDefault();
 	$(this).closest('.ff-slide-popup').removeClass('active');
 });
+
+// Close on outside click
+$('html').click(function(e) {
+	if( $(e.target).closest('.ff-slide-popup').length || $(e.target).closest('.slide-popup-show').length || $(e.target).hasClass('slide-popup-show') ) {
+		return;
+	}
+	$('.ff-slide-popup').removeClass('active');
+})
